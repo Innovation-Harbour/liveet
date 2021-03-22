125140e4a7bc62eaaf1ae2f82ee862b9d7de1e2c
kolapo <kolapo@iharbour.ng>
2021-03-22 22:58:13 +0000

    Sign Up changes

--- src/Liveet/Routes/v1/mobile/index.php
+++ src/Liveet/Routes/v1/mobile/index.php
@@ -6,6 +6,6 @@ isset($v1Group) && $v1Group->group(
     "/m",
     function (RouteCollectorProxy $mobileGroup) {
 
-        require "src/Liveet/Routes/v1/mobile/auth.php";
+        require "src/Liveet/Routes/v1/mobile/Auth.php";
     }
 );

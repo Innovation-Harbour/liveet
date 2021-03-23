18825472c6a17fff5d00217322990901a3874af6
kolapo <80823558+Innovation-Harbour@users.noreply.github.com>
2021-03-23 23:42:02 +0000

    AuthController Update

---
+++ src/Liveet/Models/Mobile/tempModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Liveet\Models\Mobile;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Event;
+use Liveet\Models\BaseModel;
+
+class TempModel extends BaseModel
+{
+    use SoftDeletes;
+
+    protected $table = "temp_user";
+    public $primaryKey = "temp_id";
+    protected $guarded = [];
+
+    public $timestamps = false;
+}
+
+ ?>

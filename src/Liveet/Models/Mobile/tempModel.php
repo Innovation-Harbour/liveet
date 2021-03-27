4f105cf3419bafbff40948dd1ab2b566592d322c
kolapo <80823558+Innovation-Harbour@users.noreply.github.com>
2021-03-27 17:05:49 +0000

    Update tempModel.php

--- src/Liveet/Models/Mobile/tempModel.php
+++ src/Liveet/Models/Mobile/tempModel.php
@@ -2,13 +2,11 @@
 
 namespace Liveet\Models\Mobile;
 
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Event;
 use Liveet\Models\BaseModel;
 
 class TempModel extends BaseModel
 {
-    use SoftDeletes;
 
     protected $table = "temp_user";
     public $primaryKey = "temp_id";

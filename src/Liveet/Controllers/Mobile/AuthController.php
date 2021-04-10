e519c48f19365b4340f991d6b04c485d91057765
kolapo <80823558+Innovation-Harbour@users.noreply.github.com>
2021-04-10 12:57:40 +0100

    Update AuthController.php

--- src/Liveet/Controllers/Mobile/AuthController.php
+++ src/Liveet/Controllers/Mobile/AuthController.php
@@ -158,18 +158,6 @@ class AuthController extends BaseController {
 
   public function VerifyOTP (Request $request, ResponseInterface $response): ResponseInterface
   {
-    $event_code = "TestEventID1234";
-
-    $result = $this->awsAddEvent($event_code);
-
-    var_dump($result);
-    die;
-
-
-
-
-
-    /*
     //declare needed class objects
     $json = new JSON();
 
@@ -199,7 +187,6 @@ class AuthController extends BaseController {
 
       return $json->withJsonResponse($response, $error);
     }
-    */
   }
 
   public function ResendOTP (Request $request, ResponseInterface $response): ResponseInterface

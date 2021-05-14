2ec77eb0d758393c6b7201649a8c4f000dcf2755
kolapo <80823558+Innovation-Harbour@users.noreply.github.com>
2021-05-15 00:09:42 +0100

    Update InvitationModel.php

--- src/Liveet/Models/InvitationModel.php
+++ src/Liveet/Models/InvitationModel.php
@@ -11,7 +11,7 @@ class InvitationModel extends BaseModel
     protected $table = "event_invitation";
     public $primaryKey = "event_invitation_id";
     protected $guarded = [];
-    protected $fillable = ['event_id','invitation_name','event_inviter','event_invitee_number','invitee_can_invite','event_invitation_status'];
+    protected $fillable = ['event_id','invitation_name','event_inviter_user_id','event_invitee_user_phone','invitee_can_invite_count','event_invitation_status'];
 
     //public $timestamps = false;
 

512b81eb94c1d3cd63ab2214a30f4936338a51d2
kolapo <80823558+Innovation-Harbour@users.noreply.github.com>
2021-05-16 18:10:52 +0100

    Update FavouriteModel.php

--- src/Liveet/Models/Mobile/FavouriteModel.php
+++ src/Liveet/Models/Mobile/FavouriteModel.php
@@ -21,8 +21,9 @@ class FavouriteModel extends BaseModel
               event.event_id,event.event_venue,event.location_lat,event.location_long,event_invitation.invitee_can_invite_count,event.event_multimedia,event.event_name,event.event_date_time,event.event_payment_type,event_control.event_can_invite,fav.event_favourite_id
               FROM event_user_favourite fav
               INNER JOIN event ON fav.event_id = event.event_id
+              LEFT JOIN user ON fav.user_id = user.user_id
               LEFT JOIN event_control ON fav.event_id = event_control.event_id
-              LEFT JOIN event_invitation ON fav.event_id = event_invitation.event_id
+              LEFT JOIN event_invitation ON fav.event_id = event_invitation.event_id and user.user_phone = event_invitation.event_invitee_user_phone
               WHERE fav.user_id = ".$user_id."
               ORDER BY event.event_date_time DESC, event.event_id LIMIT ".$offset.", ".$limit."
               ";

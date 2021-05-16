1df442c08d82daf1cca0bf16e1ee49a36a8ff5f4
kolapo <80823558+Innovation-Harbour@users.noreply.github.com>
2021-05-16 16:08:01 +0100

    Update EventMobileController.php

--- src/Liveet/Controllers/Mobile/EventMobileController.php
+++ src/Liveet/Controllers/Mobile/EventMobileController.php
@@ -659,8 +659,8 @@ class EventMobileController extends BaseController {
     $clean_phone = [];
 
     foreach($all_phones as $phone){
-      $phone = preg_replace('/[^a-zA-Z0-9-_\.]/','', $phone);
-      var_dump($phone);
+      $stripped_phone = preg_replace('/[^a-zA-Z0-9-_\.]/','', $phone);
+      var_dump($stripped_phone);
     }
 
     var_dump($all_phones);

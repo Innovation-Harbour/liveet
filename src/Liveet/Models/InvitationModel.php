c422936d0cb492791346e0fa6df39579157b36b0
kolapo <80823558+Innovation-Harbour@users.noreply.github.com>
2021-05-22 13:08:23 +0100

    Update InvitationModel.php

--- src/Liveet/Models/InvitationModel.php
+++ src/Liveet/Models/InvitationModel.php
@@ -18,14 +18,13 @@ class InvitationModel extends BaseModel
     public function getMobileEvents($user_id, $offset, $limit){
       $sql = "
               SELECT
-              event.event_id,event.event_venue,event.location_lat,event.location_long,e.invitee_can_invite_count,event.event_multimedia,event.event_name,event.event_date_time,event.event_payment_type,event_control.event_can_invite,event_user_favourite.event_favourite_id
+              event.event_id,e.event_invitation_status,event.event_venue,event.location_lat,event.location_long,e.invitee_can_invite_count,event.event_multimedia,event.event_name,event.event_date_time,event.event_payment_type,event_control.event_can_invite,event_user_favourite.event_favourite_id
               FROM event_invitation e
               LEFT JOIN user ON e.event_invitee_user_phone = user.user_phone
               RIGHT JOIN event ON e.event_id = event.event_id
               INNER JOIN event_control ON event.event_id = event_control.event_id
               LEFT JOIN event_user_favourite ON event.event_id = event_user_favourite.event_id AND event_user_favourite.user_id = ".$user_id."
               WHERE (user.user_id = ".$user_id."
-              AND e.event_invitation_status === PENDING)
               OR event.event_type = 'PUBLIC' ORDER BY user.user_id DESC, event.event_id LIMIT ".$offset.", ".$limit."
               ";
       $result = $this->getConnection()->select($sql);

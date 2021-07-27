fc19455c0889d97ebe86c725a08a50f2382e0f4e
kolapo <princeoban@users.noreply.github.com>
2021-07-28 00:28:11 +0100

    Update LiveetFunction.php

--- src/Liveet/Controllers/Mobile/Helper/LiveetFunction.php
+++ src/Liveet/Controllers/Mobile/Helper/LiveetFunction.php
@@ -316,9 +316,14 @@ trait LiveetFunction
 
     }
     catch (\Exception $e){
+      var_dump($e->getMessage());
+      die;
       return [$is_approved,$ticket_name,$user_id];
     }
-    
+
+    var_dump($img_result);
+    die;
+
     if(isset($img_result["FaceMatches"][0]["Face"]["FaceId"]))
     {
       $similarity = round($img_result["FaceMatches"][0]["Similarity"]);

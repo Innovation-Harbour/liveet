bed806ec72696c6a3cf67f69cddd83fbb6083878
kolapo <princeoban@users.noreply.github.com>
2021-08-01 14:05:45 +0100

    Update LiveetFunction.php

--- src/Liveet/Controllers/Mobile/Helper/LiveetFunction.php
+++ src/Liveet/Controllers/Mobile/Helper/LiveetFunction.php
@@ -314,47 +314,47 @@ trait LiveetFunction
 
     }
     catch (\Exception $e){
+      var_dump($e->getMessage());
+      die;
       return [$is_approved,$ticket_name,$user_id];
     }
 
     var_dump($img_result["FaceMatches"]);
-    die
+    die;
 
-    foreach ($img_result["FaceMatches"] as $facematch)
+    //foreach ($img_result["FaceMatches"] as $facematch)
+
+    if(isset($img_result["FaceMatches"][0]["Face"]["FaceId"]))
     {
+      $similarity = round($img_result["FaceMatches"][0]["Similarity"]);
 
-      if($facematch["Face"]["FaceId"])
+      if($similarity > 90)
       {
-        $similarity = round($facematch["Similarity"]);
+        $face_id = $img_result["FaceMatches"][0]["Face"]["FaceId"];
 
-        if($similarity > 90)
+        if($from_mqtt)
         {
-          $face_id = $facematch["Face"]["FaceId"];
-
-          if($from_mqtt)
-          {
-            $event_user = $event_user_db->where("user_face_id",$face_id)->where("event_ticket_id",$ticket_id)->where("ownership_status",Constants::EVENT_TICKET_ACTIVE);
-          }
-          else {
-            $event_user = $event_user_db->where("user_face_id",$face_id)->where("ownership_status",Constants::EVENT_TICKET_ACTIVE);
-          }
-
-          if($event_user->count() == 1)
-          {
-            $user_details =  $event_user->first();
-            $ticket_id = $user_details->event_ticket_id;
-            $user_id = $user_details->user_id;
-
-            $ticket_details = $ticket_db->where("event_ticket_id", $ticket_id)->first();
-            $ticket_name = $ticket_details->ticket_name;
-
-            //update the ticket as used
-            $event_user->update(["status" => Constants::EVENT_TICKET_USED]);
-            $is_approved = true;
-          }
+          $event_user = $event_user_db->where("user_face_id",$face_id)->where("event_ticket_id",$ticket_id)->where("ownership_status",Constants::EVENT_TICKET_ACTIVE);
+        }
+        else {
+          $event_user = $event_user_db->where("user_face_id",$face_id)->where("ownership_status",Constants::EVENT_TICKET_ACTIVE);
         }
 
+        if($event_user->count() == 1)
+        {
+          $user_details =  $event_user->first();
+          $ticket_id = $user_details->event_ticket_id;
+          $user_id = $user_details->user_id;
+
+          $ticket_details = $ticket_db->where("event_ticket_id", $ticket_id)->first();
+          $ticket_name = $ticket_details->ticket_name;
+
+          //update the ticket as used
+          $event_user->update(["status" => Constants::EVENT_TICKET_USED]);
+          $is_approved = true;
+        }
       }
+
     }
      return [$is_approved,$ticket_name,$user_id];
   }

03fff563d36c511eb93dbd8fc13c0cf2170e7cba
Tella Abdulrasheed <rashtell2@gmail.com>
2021-08-25 12:36:25 +0100

    fix: changed event s3 contatiner

--- src/Liveet/Controllers/EventController.php
+++ src/Liveet/Controllers/EventController.php
@@ -53,7 +53,7 @@ class EventController extends HelperController
                     [
                         "mediaKey" => "event_multimedia", "folder" => "events",
                         "clientOptions" => [
-                            "containerName" => "liveet-media", "mediaName" => rand(00000000, 99999999)
+                            "containerName" => "liveet-prod-media", "mediaName" => rand(00000000, 99999999)
                         ]
                     ]
                 ]
